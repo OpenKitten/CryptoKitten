04c357f2b6780ae966cdd7b339d08a7f63ade384
Joannis Orlandos <joannis@orlandos.nl>
2019-08-06 13:41:00 +0200

    Swift 5 support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:4.0
 import PackageDescription
 
-let package = Package(
-    name: "CryptoKitten"
+var package = Package(
+    name: "CryptoKitten",
+    products: [
+        .library(name: "CryptoKitten", targets: ["CryptoKitten"])
+    ],
+    targets: [
+        .target(name: "CryptoKitten")
+    ]
 )
